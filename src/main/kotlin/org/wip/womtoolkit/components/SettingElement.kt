5a9e03628847fd4d79666b1131771a868691ca53
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2025-06-20 19:00:47 +0200

    stuff

--- src/main/kotlin/org/wip/womtoolkit/components/SettingElement.kt
+++ src/main/kotlin/org/wip/womtoolkit/components/SettingElement.kt
@@ -3,12 +3,20 @@ package org.wip.womtoolkit.components
 import javafx.fxml.FXMLLoader
 import javafx.scene.layout.BorderPane
 
+/**
+ * @author Wip
+ * Exposes: image|svg / name/ description/ switch|choice|custom? / expandable
+ */
 class SettingElement : BorderPane() {
-	init {
-		FXMLLoader(javaClass.getResource("/components/settingElement.fxml")).apply {
-			setRoot(this@SettingElement)
-			setController(this@SettingElement)
-			load()
-		}
-	}
+    init {
+        try {
+            FXMLLoader(javaClass.getResource("/components/settingElement.fxml")).apply {
+                setRoot(this@SettingElement)
+                setController(this@SettingElement)
+                load()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
